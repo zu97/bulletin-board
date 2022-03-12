20fe4e0907c45b58e159ada8bb0b69c13f15d694
admin <admin@admin.com>
2022-03-12 13:42:47 +0600

    Initial setup and creation of routing for the frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponents } from './page.not-found.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/page.not-found.components.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `<h3 class="not-found">Page not found!</h3>`,
+  styles: ['.not-found { padding: 20px; text-align: center }']
+})
+export class PageNotFoundComponents {}
