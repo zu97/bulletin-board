77b50962959adf1f8f91f6d7b8a0b2371e3fa4df
admin <admin@admin.com>
2022-03-12 14:07:26 +0600

    Creating a ui component to center the card for the frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,18 +8,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponents } from './page.not-found.components';
 import { LayoutComponent } from './ui/layout/layout.component';
 import { LoaderComponent } from './ui/loader/loader.component';
+import { CenteredCardComponent } from './ui/centered-card/centered-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponents,
     LayoutComponent,
-    LoaderComponent
+    LoaderComponent,
+    CenteredCardComponent
   ],
   imports: [
     BrowserModule,
