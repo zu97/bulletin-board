4d96489d790e20bd9a0ce3c5828f9d55f5550e52
admin <admin@admin.com>
2022-03-12 14:34:03 +0600

    Updating the layout changing buttons depending on the login for frontend

--- frontend/src/app/ui/layout/layout.component.ts
+++ frontend/src/app/ui/layout/layout.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.model';
+import { AppState } from '../../store/types';
+import { Store } from '@ngrx/store';
+import { logoutUserRequest } from '../../store/users.actions';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  user: Observable<null | User>;
+
+  constructor(
+    private store: Store<AppState>,
+  ) {
+    this.user = store.select(state => state.users.user);
+  }
+
+  logout() {
+    this.store.dispatch(logoutUserRequest());
+  }
+}
